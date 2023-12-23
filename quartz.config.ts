dc1bcf980225f94e08a60eb5ae138432bc7e91ff
Pranav Mandhare <pranavmandhare27@gmail.com>
2023-12-24 01:53:26 +0530

    subdomain

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "garden.pranavmandhare.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
