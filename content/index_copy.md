6d35f81ee01697beba9117bebf9d994e2cdb564e
Pranav Mandhare <pranav@Infinitys-MacBook-Air.local>
2023-12-24 00:32:53 +0530

    quartz setup

---
+++ content/index_copy.md
@@ -0,0 +1,2 @@
+This is an Obsidian blog, or a Knowledge Graph exported as a website. An open-source repo, Quartz 4.0 was used to build this project.
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Pranav Mandhare",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
